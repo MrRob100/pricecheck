d459223ac683196bf02855defd87e9db8a548a43
horidhenry <robarnold100@hotmail.com>
2021-12-01 13:16:46 -0300

    dddddd

--- app/Http/Controllers/PairsController.php
+++ app/Http/Controllers/PairsController.php
@@ -8,9 +8,23 @@ class PairsController extends Controller
 {
     public function update(UpdatePairsRequest $request): bool
     {
+        dump($request->all());
+
         if ($request->pairs) {
-            file_put_contents(public_path() . '/pairs.json', $request->pairs);
+
+            dump('pairs');
+
+            try {
+                file_put_contents(public_path() . '/pairs.json', $request->pairs);
+            }
+            catch(\Exception $e) {
+                dump($e->getMessage());
+            }
+
         } else {
+
+            dump('not pairs');
+
             return false;
         }
 
